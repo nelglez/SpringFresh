e675fe58f1948bd04c9c2536f4bd1827bbdf53d9
Nelson Gonzalez <nelglezfl@gmail.com>
2019-03-18 08:14:35 -0400

    Initial Commit

---
+++ SpringFresh/DesignableView.swift
@@ -0,0 +1,36 @@
+//
+//  DesignableView.swift
+//  SpringFresh
+//
+//  Created by Nelson Gonzalez on 3/18/19.
+//  Copyright © 2019 Nelson Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+    
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+    
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+    
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+}
